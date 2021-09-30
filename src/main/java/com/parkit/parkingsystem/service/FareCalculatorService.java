1fd1bd49fcdfc3deaacd3b40e7e23ec4947e9f74
Mo-BiuS <lodland95@gmail.com>
2021-09-30 11:54:19 +0200

    TODO#1 : Fix the code to make the unit tests pass
    The problem was as follow :
    Only took in charge hour of the date,
    So i integrated a function to calculate the total number of hour between
    in and out including minutes, days, month and years.

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,20 +1,26 @@
 package com.parkit.parkingsystem.service;
 
+import java.util.Date;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
-
+	private static final Logger logger = LogManager.getLogger("FareCalculator");
+	
     public void calculateFare(Ticket ticket){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
-
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
-
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        
+        double inHour = dateToHour(ticket.getInTime());
+        double outHour = dateToHour(ticket.getOutTime());
+        double duration = outHour - inHour;
+        
+        logger.info("inHour : " + inHour + " | outHour : " + outHour + " | duration : " + duration);
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
@@ -27,5 +33,31 @@ public class FareCalculatorService {
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
         }
+    }
+    
+    /**
+     * @param d Date
+     * @return double representing the number of hour elapsed from the 1st January 2000.
+     */
+    protected double dateToHour(Date d) {
+    	double year = d.getYear()-100;
+    	if(year % 4 == 0) year *= 8784;
+    	else year *= 8760;
+    	
+    	double month = d.getMonth();
+    	if(month == 2) {
+        	if(year % 4 == 0) month *= 696;
+        	else month *= 672;
+    	}
+    	else if(month % 2 == 0) month *= 720;
+    	else month *= 744;
+    	
+    	double day = d.getDay()*24;
+    	
+    	double hour = d.getHours();
+    	
+    	double minute = ((double)d.getMinutes())/60;
+    	
+    	return year + month + day + hour + minute;
     }
 }
